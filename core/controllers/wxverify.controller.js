db7d30884131ee29491936fb5f9ed30617bd25f6
lbgongfu <lbgongfu@163.com>
2019-03-14 20:59:51 +0800

    新增微信验证接口

---
+++ core/controllers/wxverify.controller.js
@@ -0,0 +1,14 @@
+var async = require('async');
+var _ = require('lodash');
+var siteInfoService = require('../services/site-info.service');
+var categoriesService = require('../services/categories.service');
+var listsService = require('../services/lists.service');
+
+/**
+ * 首页
+ * @param {Object} req
+ * @param {Object} res
+ */
+module.exports = function(req, res) {
+    res.render('kWRhf3OTDzhrDRhc');
+};
